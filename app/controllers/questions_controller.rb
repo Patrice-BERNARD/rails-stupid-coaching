48b4fb30a89f8a4ab03204b326b3e8a71886e672
patrice bernard <patrice.a.bernard@laposte.fr>
2026-05-18 14:10:17 +0200

    rails stupid coachin v2

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,14 +1,13 @@
 class QuestionsController < ApplicationController
 
   def ask
-    @question = params[:query]
   end
 
   def answer
-
-    if params[:query].gsub("+"," ") == "I am going to work"
+    @question = params[:query]
+    if @question == "I am going to work"
       @answer = "Great!"
-    elsif params[:query].gsub("+"," ").end_with?("?")
+    elsif @question.end_with?("?")
      @answer = "Silly question, get dressed and go to work!"
     else
       @answer = "I don't care, get dressed and go to work!"
